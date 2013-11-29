4e59e21c46bd382a9d865d2afc956b0938ed39b8
Don Gilbert <don@electriceasel.com>
2013-11-29 12:29:48 -0600

    Oops

--- www/index.php
+++ www/index.php
@@ -5,10 +5,8 @@ require __DIR__ . "/../boot.php";
 use Joomla\DI\Container;
 use Stats\Providers;
 
-$config = include APPROOT . "/etc/config.php";
-
 $container = (new Joomla\DI\Container)
-	->registerServiceProvider(new Providers\ConfigServiceProvider($config))
+	->registerServiceProvider(new Providers\ConfigServiceProvider(APPROOT . "/etc/config.php"))
 	->registerServiceProvider(new Providers\DatabaseServiceProvider);
 
 
