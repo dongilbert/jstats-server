ba4897c80925489245cf96abf457924b5060a6bb
Don Gilbert <don@electriceasel.com>
2013-12-01 22:25:31 -0600

    Add mysql db schema

---
+++ etc/mysql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `jos_jstats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `unique_id` varchar(32) NOT NULL,
+  `php_version` varchar(255) NOT NULL,
+  `db_type` varchar(255) NOT NULL,
+  `db_version` varchar(255) NOT NULL,
+  `cms_version` varchar(255) NOT NULL,
+  `server_os` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
