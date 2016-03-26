297a4fe5df0a24808dfa811a075fa5c8fd89fffd
Michael Babker <mbabker@flbab.com>
2016-03-25 20:49:52 -0400

    Add indexes

--- etc/mysql.sql
+++ etc/mysql.sql
@@ -6,5 +6,10 @@ CREATE TABLE IF NOT EXISTS `jos_jstats` (
   `cms_version` varchar(15) NOT NULL,
   `server_os` varchar(255) NOT NULL,
   `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`unique_id`)
+  PRIMARY KEY (`unique_id`),
+  KEY `idx_php_version` (`php_version`),
+  KEY `idx_db_type` (`db_type`),
+  KEY `idx_db_version` (`db_version`),
+  KEY `idx_cms_version` (`cms_version`),
+  KEY `idx_server_os` (`server_os`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
