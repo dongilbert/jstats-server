72b09acc56881a6478d31e200580855cc0502f05
Michael Babker <mbabker@flbab.com>
2016-03-25 20:45:06 -0400

    Shorten the database columns

--- etc/mysql.sql
+++ etc/mysql.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `jos_jstats` (
   `unique_id` varchar(40) NOT NULL,
-  `php_version` varchar(255) NOT NULL,
-  `db_type` varchar(255) NOT NULL,
-  `db_version` varchar(255) NOT NULL,
-  `cms_version` varchar(255) NOT NULL,
+  `php_version` varchar(15) NOT NULL,
+  `db_type` varchar(15) NOT NULL,
+  `db_version` varchar(50) NOT NULL,
+  `cms_version` varchar(15) NOT NULL,
   `server_os` varchar(255) NOT NULL,
   `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`unique_id`)
