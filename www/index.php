b2bafb1e6843675e45802563ed905471630184e3
Michael Babker <mbabker@flbab.com>
2017-02-26 18:36:07 -0600

    A couple more file doc blocks

--- www/index.php
+++ www/index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Joomla! Statistics Server
+ *
+ * @copyright  Copyright (C) 2013 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
+ */
 
 require dirname(__DIR__) . '/boot.php';
 
